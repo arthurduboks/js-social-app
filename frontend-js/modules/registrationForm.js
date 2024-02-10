ad560deb66e8326482a5ccbb7217208a3bea7882
Arthur Duboks <duboks.arthur@gmail.com>
2024-02-10 11:50:28 -0500

    validation logic init

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -24,17 +24,52 @@ export default class RegistrationForm {
   }
 
   usernameHandler() {
+    this.username.errors = false;
     this.usernameInitial();
     clearTimeout(this.username.timer);
     this.username.timer = setTimeout(() => this.usernameDelay(), 800);
   }
 
   usernameInitial() {
-    console.log("Initial ran.");
+    if (
+      this.username.value != "" &&
+      !/^([a-zA-Z0-9])+$/.test(this.username.value)
+    ) {
+      this.showValidationErr(
+        this.username,
+        "Username can only contain letters and numbers."
+      );
+    }
+
+    if (this.username.value.length > 30) {
+      this.showValidationErr(
+        this.username,
+        "Username can not exceed 30 characters."
+      );
+    }
+
+    if (!this.username.errors) {
+      this.hideValidationErr(this.username);
+    }
+  }
+
+  hideValidationErr(el) {
+    el.nextElementSibling.classList.remove("liveValidateMessage--visible");
+  }
+
+  showValidationErr(el, message) {
+    el.nextElementSibling.innerHTML = message;
+    el.nextElementSibling.classList.add("liveValidateMessage--visible");
+    el.errors = true;
   }
 
   usernameDelay() {
-    alert("Delay ran.");
+    if (this.username.value.length < 5) {
+      this.showValidationErr(
+        this.username,
+        "Username must be at least 5 characters. "
+      );
+    }
   }
 
   insertValidation(el) {
